fda031a47119a23b6a5f02febe6d2b97622894ec
Dan Fockler <dfockler13@gmail.com>
2016-02-05 19:48:21 -0800

    finished dot product and updated example

--- examples/basic.rs
+++ examples/basic.rs
@@ -18,4 +18,33 @@ fn main() {
     let product = matrix.mult(&mata);
     println!("{:?}", sum);
     println!("{:?}", product);
+
+    let mut mat = Matrix::new(2, 4);
+    mat.push(1, 1, 1);
+    mat.push(2, 1, 2);
+    mat.push(3, 1, 3);
+    mat.push(4, 1, 4);
+    mat.push(5, 2, 1);
+    mat.push(6, 2, 2);
+    mat.push(7, 2, 3);
+    mat.push(8, 2, 4);
+    let transpose_a = mat.transpose();
+    println!("{:?}", mat.index(2, 1));
+    println!("{:?}", transpose_a);
+    println!("{:?}", transpose_a.index(2, 1));
+
+    let mut other = Matrix::new(4, 2);
+    other.push(1, 1, 1);
+    other.push(2, 2, 1);
+    other.push(3, 3, 1);
+    other.push(4, 4, 1);
+    other.push(5, 1, 2);
+    other.push(6, 2, 2);
+    other.push(7, 3, 2);
+    other.push(8, 4, 2);
+    println!("mat > {:?}", mat);
+    println!("other > {:?}", other);
+    let result = mat.dot(&other);
+    println!("{:?}", result);
+    println!("{:?}", result.index(2, 2));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ extern crate num;
 use num::Num;
 use num::Zero;
 
+/// The struct used by the Matrix type is simple. It uses the number of rows,
+/// the number of columns and a data field that contains the data.
 #[derive(Debug)]
 pub struct Matrix<T> {
     rows: usize,
@@ -16,25 +18,27 @@ pub struct Matrix<T> {
     data: Vec<T>,
 }
 
-// trait MatrixOps<T> {
-//     fn add(&self, m: &Matrix<T>) -> Matrix<T>;
-//     // fn mult(&self, m: &Matrix<T>) -> Matrix<T>;
-//     // fn dot(&self, m: &Matrix<T>) -> Matrix<T>;
-//     // fn scalar(&self, scalar: T) -> Matrix<T>;
-//     // fn transpose(&self) -> Matrix<T>;
-//     fn index(&self, i: usize, j: usize) -> T;
-//     // fn push(&mut self, value: T, i: usize, j: usize); 
-// }
-
+/// The Matrix type allows for simple operations on an underlying Vec, as if
+/// the data was arranged in a 2D array. None of the functions in the library
+/// modify the Matrix, with the exception of `push`
+///
+/// The Matrix type is generic over Numeric types that can be cloned
 impl<T: Num + Clone> Matrix<T> {
+    /// Returns a new Matrix with the given number of rows and columns set.
+    /// All elements are set to zero
     pub fn new(rows: usize, cols: usize) -> Matrix<T> {
         Matrix { 
-            rows: rows, 
-            cols: cols, 
+            rows: rows,
+            cols: cols,
             data: vec![T::zero(); cols * rows],
         }
     }
 
+    /// Returns a new Matrix with the number of rows and columns set.
+    /// Uses the provided data to set the data in the Matrix
+    ///
+    /// TODO: Make this public and check data is the correct size
+    /// given the rows/cols
     fn new_with_data(rows: usize, cols: usize, data: Vec<T>) -> Matrix<T>{
         Matrix {
             rows: rows,
@@ -43,6 +47,9 @@ impl<T: Num + Clone> Matrix<T> {
         }
     }
 
+    /// Performs an element-wise addition onto the Matrix
+    ///
+    /// TODO: Add error handling to ensure size compatibility
     pub fn add(&self, m: &Matrix<T>) -> Matrix<T> {
         let mut vec: Vec<T> = Vec::new(); 
         for (a, b) in self.data.iter().zip(m.data.iter()) {
@@ -51,6 +58,9 @@ impl<T: Num + Clone> Matrix<T> {
         Matrix::new_with_data(self.rows, self.cols, vec)
     }
 
+    /// Performs an element-wise multiplication onto the Matrix
+    ///
+    /// TODO: Add error handling to ensure size compatibility
     pub fn mult(&self, m: &Matrix<T>) -> Matrix<T> {
         let mut vec: Vec<T> = Vec::new(); 
         for (a, b) in self.data.iter().zip(m.data.iter()) {
@@ -59,16 +69,48 @@ impl<T: Num + Clone> Matrix<T> {
         Matrix::new_with_data(self.rows, self.cols, vec)
     }
 
+    /// Transposes the Matrix and returns the resulting Matrix
+    pub fn transpose(&self) -> Matrix<T> {
+        let mut vec: Vec<T> = Vec::new();
+        for i in 0..self.cols {
+            for j in 0..self.rows {
+                vec.push(self.data[(j * self.cols + i)].clone());
+            }
+        }
+        Matrix::new_with_data(self.cols, self.rows, vec)
+    }
+
+    /// Performs the dot product and returns the resulting Matrix
+    /// TODO: Add error handling to ensure size compatibility
+    pub fn dot(&self, m: &Matrix<T>) -> Matrix<T> {
+        let mut vec: Vec<T> = Vec::new();
+        for i in 0..self.rows {
+            for j in 0..m.cols {
+                let mut sum = T::zero();
+                for k in 0..self.cols {
+                    sum = sum + self.data[i * self.cols + k].clone() * m.data[k * m.cols + j].clone();
+                }
+                vec.push(sum);
+            }
+        }
+        Matrix::new_with_data(self.rows, m.cols, vec)
+    }
+
+    /// Returns the dimensions of the Matrix in a tuple
+    /// in (rows, columns) format
     pub fn dimensions(&self) -> (usize, usize) {
         (self.rows, self.cols)
     }
 
+    /// Returns the specific value at an index given by
+    /// a row and column
     pub fn index(&self, row: usize, col: usize) -> T {
-        self.data[(row * col) - 1].clone()
+        self.data[(row-1) * self.cols + (col-1)].clone()
     }
 
+    /// Pushes a value into the specified location in the Matrix
     pub fn push(&mut self, value: T, row: usize, col: usize) {
-        self.data[(row * col) - 1] = value;
+        self.data[ (row-1) * self.cols + (col-1) ] = value;
     }
 }
 
